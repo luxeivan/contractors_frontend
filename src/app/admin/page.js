871c55fbcc41deae66aadcf761a4a5019715bda6
luxeivan <luxeivan@gmail.com>
2025-04-06 21:27:29 +0300

    second commit

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,11 +1,16 @@
 import { cookies } from 'next/headers'
 import React from 'react'
+import Title from 'antd/es/typography/Title'
+import Text from 'antd/es/typography/Text'
 
 export default function Admin() {
     const user = JSON.parse(cookies().get('user')?.value)
     console.log("user", user);
 
     return (
-        <div>Admin page</div>
+        <div>
+            <Title>Панель администратора</Title>
+            <Text>Добро пожаловать {user.username}</Text>
+        </div>
     )
 }
