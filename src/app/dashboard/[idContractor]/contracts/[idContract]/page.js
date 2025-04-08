0901e6156b7fabbf7b95eaa7651eb34739d23fee
luxeivan <luxeivan@gmail.com>
2025-04-08 12:49:17 +0300

    сделал отображение фото в этапе

--- src/app/dashboard/[idContractor]/contracts/[idContract]/page.js
+++ src/app/dashboard/[idContractor]/contracts/[idContract]/page.js
@@ -1,23 +1,28 @@
 // import { cookies } from 'next/headers'
-import { Button, Card, Collapse, Flex } from 'antd'
+import { Button, Card, Collapse, Flex, Image } from 'antd'
 import Title from 'antd/es/typography/Title'
 // import axios from 'axios'
 import React from 'react'
 import { getContractItem } from '@/lib/getData'
 import ButtonAddStep from '@/components/ButtonAddStep'
-// const server = process.env.SERVER_API
+const server = process.env.SERVER_API
 export default async function Contract({ params }) {
     // const jwt = (await cookies()).get('jwt')?.value || null
     const { idContractor, idContract } = await params
     let contract = await getContractItem(idContract)
-    
+
     console.log("contract:", contract);
     const countSteps = contract.steps.length
     const items = contract.steps.map((item, index) => (
         {
             key: index + 1,
             label: item.name,
-            children: <p>{item.description}</p>,
+            children: <Flex vertical gap={20}>
+                <p>{item.description}</p>
+                <Flex gap={20}>
+                    {item.photos.map(item => <Image src={`${server}${item.url}`} width={200}/>)}
+                </Flex>
+            </Flex>,
         }
     ))
 
@@ -28,7 +33,7 @@ export default async function Contract({ params }) {
                     <Title>Договор №{contract.number}</Title>
                     <Flex gap={20} vertical>
                         <Flex justify='end'>
-                            <ButtonAddStep idContract={idContract} countSteps={countSteps}/>
+                            <ButtonAddStep idContract={idContract} countSteps={countSteps} />
                         </Flex>
                         <Collapse items={items} defaultActiveKey={[items.length]} />
                     </Flex>
