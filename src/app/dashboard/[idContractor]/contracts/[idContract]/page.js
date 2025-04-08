d627894c7b48e2caae085f5d22ae45d25354d1fb
luxeivan <luxeivan@gmail.com>
2025-04-08 15:12:00 +0300

    добавил дату

--- src/app/dashboard/[idContractor]/contracts/[idContract]/page.js
+++ src/app/dashboard/[idContractor]/contracts/[idContract]/page.js
@@ -1,10 +1,12 @@
 // import { cookies } from 'next/headers'
 import { Button, Card, Collapse, Flex, Image } from 'antd'
 import Title from 'antd/es/typography/Title'
+import Text from 'antd/es/typography/Text'
 // import axios from 'axios'
 import React from 'react'
 import { getContractItem } from '@/lib/getData'
 import ButtonAddStep from '@/components/ButtonAddStep'
+import dayjs from 'dayjs'
 const server = process.env.NEXT_PUBLIC_SERVER_API
 export default async function Contract({ params }) {
     // const jwt = (await cookies()).get('jwt')?.value || null
@@ -16,11 +18,11 @@ export default async function Contract({ params }) {
     const items = contract.steps.map((item, index) => (
         {
             key: index + 1,
-            label: item.name,
+            label: <Flex gap={30}><Text>{item.name}</Text><Text><span style={{color:"gray"}}>Дата создания: {dayjs(item.createdAt).format('DD-MM-YYYY HH:mm')}</span></Text></Flex>,
             children: <Flex vertical gap={20}>
                 <p>{item.description}</p>
-                <Flex gap={20}>
-                    {item.photos.map(item => <Image key={item.id} src={`${server}${item.url}`} width={200}/>)}
+                <Flex gap={20}>                
+                        {item.photos.map(item => <Image key={item.id} src={`${server}${item.url}`} width={200} />)}                             
                 </Flex>
             </Flex>,
         }
