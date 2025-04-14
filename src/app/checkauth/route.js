170c832afdef357b6ed8929e644a21926697cd5c
Иван Шишкин <luxeivan@gmail.com>
2025-04-14 12:18:14 +0300

    ссылки поправил

--- src/app/checkauth/route.js
+++ src/app/checkauth/route.js
@@ -5,12 +5,12 @@ export async function GET(request) {
     const session = await auth()
     console.log("session",session);
     if(!session){
-        return NextResponse.redirect(new URL('/login', request.url))
+        return NextResponse.redirect(new URL('/login', request.nextUrl.origin))
     }else{
         if(session.user?.role==='admin'||session.user?.role==='readadmin'){
-            return NextResponse.redirect(new URL('/admin', request.url))
+            return NextResponse.redirect(new URL('/admin', request.nextUrl.origin))
         }else{
-            return NextResponse.redirect(new URL('/dashboard', request.url))
+            return NextResponse.redirect(new URL('/dashboard', request.nextUrl.origin))
         }
     }
     
