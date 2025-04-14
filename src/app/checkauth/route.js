233ed09852be0364a08fc9c4929d73c048ab127f
Иван Шишкин <luxeivan@gmail.com>
2025-04-14 13:15:33 +0300

    test

--- src/app/checkauth/route.js
+++ src/app/checkauth/route.js
@@ -3,14 +3,15 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
     const session = await auth()
-    console.log("session",session);
+    const url = new URL('/login', request.url)
+    console.log("url",url);
     if(!session){
-        return NextResponse.redirect(new URL('/login', request.nextUrl.origin))
+        return NextResponse.redirect(url)
     }else{
         if(session.user?.role==='admin'||session.user?.role==='readadmin'){
-            return NextResponse.redirect(new URL('/admin', request.nextUrl.origin))
+            return NextResponse.redirect(new URL('/admin', request.url))
         }else{
-            return NextResponse.redirect(new URL('/dashboard', request.nextUrl.origin))
+            return NextResponse.redirect(new URL('/dashboard', request.url))
         }
     }
     
