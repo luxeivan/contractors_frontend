dba5bef6dc03eda35ee0b60cf0121c22e75bb40a
luxeivan <luxeivan@gmail.com>
2025-04-15 09:31:11 +0300

    чтото сохранил

--- src/app/checkauth/route.js
+++ src/app/checkauth/route.js
@@ -1,25 +1,26 @@
 import { auth } from "@/auth";
 import { NextResponse } from "next/server";
-import { redirect } from 'next/navigation'
+// import { redirect } from 'next/navigation'
 
-export async function GET(request) {
+export async function GET(req,res) {
     const session = await auth()
-    const host = request.headers.get('host')
+    const host = req.nextUrl.origin
     console.log("host", host);
     // const url = new URL('/login', request.url)
     // console.log("url",url);
     if (!session) {
         // return NextResponse.redirect(url)
-        return NextResponse.redirect(new URL('/login', "http://" + host))
+        return NextResponse.redirect(new URL('/login', req.nextUrl.origin))
         // redirect('/login')
     } else {
         if (session.user?.role === 'admin' || session.user?.role === 'readadmin') {
-            return NextResponse.redirect(new URL('/admin', "http://" + host))
+            return NextResponse.redirect(new URL('/admin', req.nextUrl.origin))
             // redirect('/admin')
         } else {
-            return NextResponse.redirect(new URL('/dashboard', "http://" + host))
+            return NextResponse.redirect(new URL('/dashboard', req.nextUrl.origin))
             // redirect('/dashboard')
         }
     }
+    
 
 }
--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,27 @@
-export { auth as middleware} from "@/auth"
+// export { auth as middleware} from "@/auth"
 
-// export default auth((req) => {
-//     if (!req.auth && req.nextUrl.pathname !== "/login") {
-//       const newUrl = new URL("/login", req.nextUrl.origin)
-//       return Response.redirect(newUrl)
-//     }
-//   })
+import { NextResponse } from "next/server"
+import { auth } from "@/auth";
+
+export async function middleware(req) {
+  const session = await auth()
+  const host = req.nextUrl.origin
+  console.log("host123123", host);
+  if (!session) {
+    // return NextResponse.redirect(url)
+    return NextResponse.redirect(new URL('/login', req.nextUrl.origin))
+    // redirect('/login')
+  } else {
+    if (session.user?.role === 'admin' || session.user?.role === 'readadmin') {
+      return NextResponse.redirect(new URL('/admin', req.nextUrl.origin))
+      // redirect('/admin')
+    } else {
+      return NextResponse.redirect(new URL('/dashboard', req.nextUrl.origin))
+      // redirect('/dashboard')
+    }
+  }
+}
 
 export const config = {
-    matcher: ['/admin/:path*', '/dashboard/:path*','/checkauth/:path*'],
-  }
+  matcher: ['/admin/:path*', '/dashboard/:path*', '/checkauth/:path*'],
+}
